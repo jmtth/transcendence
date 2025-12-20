140566ddee3d1925bceb529ccd1025fc3874d3fc
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-12-21 00:37:35 +0100

    feat: basic display for profile - with vite (not tested on prod) - need form, buttons and styling

---
+++ srcs/nginx/src/App.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { NavBar } from "./components/NavBar"
+import { MePage } from "./pages/MePage"
+
+export const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-950 text-slate-100">
+      <NavBar />
+      <main className="min-h-screen bd-slate-950 text-slate-100">
+        <Routes>
+          <Route path="/me" element={<MePage/>}></Route>
+          <Route path="/" element={<Navigate to="/me" replace/>}></Route>
+        </Routes>
+      </main>
+    </div>
+  )
+}
