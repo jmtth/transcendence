97417af28f29ec29b5c7f82b585b1360f327a99a
Francois Petit <fpetit@2G6.42angouleme.fr>
2026-01-03 22:41:52 +0100

    fix(UM): tests mock

--- srcs/users/test/fixtures/profiles.fixtures.ts
+++ srcs/users/test/fixtures/profiles.fixtures.ts
@@ -1,11 +1,11 @@
 export const mockProfileDTO = {
-  id: 1,
+  authId: 1,
   username: 'toto',
   avatarUrl: 'default.png',
 };
 
 export const mockProfileDTO2 = {
-  id: 2,
+  authId: 2,
   username: 'tata',
   avatarUrl: 'default.png',
 };
