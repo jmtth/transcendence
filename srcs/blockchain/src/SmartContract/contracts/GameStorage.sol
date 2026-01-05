78f8dea21d45035fbd75ecdff0703f8a3f125db7
jhervoch <marvin@42fr>
2026-01-05 14:55:28 +0100

    update : add business hash , and counter for number of transaction and required tests

--- srcs/blockchain/src/SmartContract/contracts/GameStorage.sol
+++ srcs/blockchain/src/SmartContract/contracts/GameStorage.sol
@@ -13,21 +13,28 @@ contract GameStorage is Ownable {
         uint32 player4;
         uint32 timestamp;
     }
-    uint256 public tournamentCount;
     mapping(uint32 => Tournament) public tournaments;
+    uint256[] public tournamentIds;
+    mapping(uint256 => bool) public exists;
 
-    event TournamentStored(uint32 match_id, uint32 player1, uint32 player2, uint32 player3, uint32 player4);
+    event TournamentStored(uint32 match_id, uint32 player1, uint32 player2, uint32 player3, uint32 player4, uint32 ts, bytes32 snapshotHash);
 
     function storeTournament(uint32 id, uint32 p1, uint32 p2, uint32 p3, uint32 p4) external onlyOwner {
-        require(tournaments[id].timestamp == 0, "ID already used");
-        tournaments[id] = Tournament(p1, p2, p3, p4, uint32(block.timestamp));
-        emit TournamentStored(tournamentCount, id, p1, p2, p3, p4);
-        
-        tournamentCount++;
+        require(!exists[id], "Tournament already stored!");
+        uint32 ts = uint32(block.timestamp);
+        exists[id] = true;
+        tournaments[id] = Tournament(p1, p2, p3, p4, ts);
+        bytes32 snapshotHash = keccak256(abi.encode(id, p1, p2, p3, p4, ts));
+        emit TournamentStored(id, p1, p2, p3, p4, ts, snapshotHash);
+        tournamentIds.push(id); 
     }
 
     function getTournament(uint32 id) external view returns (Tournament memory){
-        require(tournaments[id].timestamp != 0, "Tournament does not exist");
+        require(exists[id], "Tournament does not exist!");
         return tournaments[id];
+    }
+    
+    function getNbTournamentStored() external view returns (uint256){
+        return tournamentIds.length;
     }
 }
