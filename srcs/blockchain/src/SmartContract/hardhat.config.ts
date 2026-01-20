83effae08f2dc7672ed8313e132d369511ae1302
jmkko <jmworld@gmail.com>
2026-01-20 08:12:01 +0100

    feat(block): config deploy on Fuji tesnet
    fix(block): ubpdate some static file lonks
    feat(block): add background of front in Dapp
    
    srcs/.env.blockchain.example
    srcs/blockchain/.env.test.blockchain
    srcs/blockchain/src/SmartContract/hardhat.config.ts
    srcs/blockchain/src/config/env.ts
    srcs/blockchain/src/core/error.ts
    srcs/blockchain/src/module/block.consumers.ts
    srcs/blockchain/src/public/bg.svg
    srcs/blockchain/src/public/output.css
    srcs/blockchain/src/public/style-block.css
    srcs/blockchain/src/views/header.ejs
    srcs/blockchain/src/views/index.ejs
    srcs/blockchain/src/views/snapshot-details.ejs
    srcs/blockchain/src/views/snapshots.ejs

--- srcs/blockchain/src/SmartContract/hardhat.config.ts
+++ srcs/blockchain/src/SmartContract/hardhat.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "hardhat/config";
-import hardhatIgnitionViemPlugin from "@nomicfoundation/hardhat-ignition-viem";
-
+import { defineConfig, configVariable } from 'hardhat/config';
+import hardhatIgnitionViemPlugin from '@nomicfoundation/hardhat-ignition-viem';
+import { env } from '../config/env.js';
 
 export default defineConfig({
   plugins: [hardhatIgnitionViemPlugin],
   solidity: {
-    version: "0.8.28",
+    version: '0.8.28',
   },
   networks: {
     localhost: {
-      url: "http://0.0.0.0:8545"
-    }
+      url: 'http://0.0.0.0:8545',
+    },
+    fuji: {
+      type: 'http',
+      url: env.AVALANCHE_RPC_URL,
+      accounts: [configVariable('AVALANCHE_PRIVATE_KEY')],
+    },
   },
 });
